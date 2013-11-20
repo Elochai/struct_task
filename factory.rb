54459c5a7ae0e2462e3bfd28294fe4ea95c0d14f
Eko <eko@eko-lenovo.(none)>
2013-11-20 20:55:26 +0200

    almost done

--- factory.rb
+++ factory.rb
@@ -3,20 +3,24 @@ class Factory
   def initialize(*args)
     Customer.class_eval do
       define_method "initialize" do |*elems|
-      	args.each_with_index do |arg,index|
-      	  elems.each_with_index do |elem, index2|
-      	  	if index2 == index
-      	  self.public_send("#{arg}=",elem[index2])
-      	end
-      	end
+        elems.each_with_index do |elem, index|
+      	  self.public_send("#{args[index]}=",elem)
         end
-	  end
-	  define_method "[]" do |arg|
-        self.public_send(arg)
-	  end
-	  args.each do |arg|
-		attr_accessor arg.to_sym
-	  end
+	  	end
+	  	define_method "[]" do |arg|
+	  		begin
+	  			if arg.class == Fixnum
+	  				self.public_send("#{args[arg]}")
+	  			else
+      			self.public_send(arg)
+     	 		end
+     	  rescue
+     	  	puts "Invalid argument"
+     	  end
+     	end
+	  	args.each do |arg|
+		  	attr_accessor arg.to_sym
+	  	end
     end
   end
 
@@ -28,6 +32,6 @@ end
  
 
 Сustomer = Factory.new(:name, :address, :zip)
-joe = Customer.new("Joe", "Maple st.")
-puts joe.name
+joe = Customer.new("Joe", "Maple st.", 12345)
+puts joe[3]
 
