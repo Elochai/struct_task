c50891696510817c5235c6bdd9926267023a7607
Eko <eko@eko-lenovo.(none)>
2013-11-20 19:47:24 +0200

    refactoring

--- factory.rb
+++ factory.rb
@@ -1,42 +1,26 @@
 class Factory 
 
-	def initialize(name, address, zip)
-		@name = name
-		@address = address
-		@zip = Integer(zip)
-		@args = [name, address, zip]
-		greeting
-	end
-
-	def name
-		puts @name
-	end
-
-	def address
-		puts @address
-	end
-
-	def zip
-		puts @zip
-	end
-
-	def greeting
-		puts "Hello, #{@name}!"
-	end 
-
-	def [] (arg)
-		if arg.class == Symbol || arg.class == String
-		  self.public_send(arg)
-	    elsif arg.class == Fixnum
- 		  puts "#{@args[arg]}"  
-	    else puts "Invalid argument class"
+  def initialize(*args)
+    Customer.class_eval do
+      define_method "initialize" do |*elems|
+	  end
+	  args.each do |arg|
+		attr_accessor arg.to_sym
+		define_method "[]" do |arg|
+          self.public_send(arg)
 	    end
-	end
+	  end
+    end
+  end
 
+end
 
-end 
+class Customer
 
-Customer = Factory.new("Joe", "Mapple st.", 1234)
-Customer[:name]
-Customer["address"]
-Customer[2]
+end
+ 
+
+Сustomer = Factory.new(:name, :address, :zip)
+joe = Customer.new("Joe", "Maple st.")
+puts joe.name
+
